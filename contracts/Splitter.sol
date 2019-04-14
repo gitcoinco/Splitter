c89d46a21f3fa2cb8e8e7a433701f92b2d6a6615
Dan Lipert <danlipert@gmail.com>
2019-04-15 03:59:49 +0900

    add require, WIP

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -3,10 +3,13 @@ import "./ERC20.sol";
 
 
 contract Splitter {
+    using SafeMath for uint256;
+
    // the splitter is stateless so there is no constructor
 
     function splitTransfer(address toFirst, address toSecond, uint256 valueFirst, uint256 valueSecond,
         address tokenAddress) external {
+        require(valueFirst > 0 && valueSecond > 0 && ERC20(tokenAddress).balanceOf(msg.sender) >= valueFirst.safeAdd(valueSecond));
         ERC20(tokenAddress).transferFrom(msg.sender, toFirst, valueFirst);
         ERC20(tokenAddress).transferFrom(msg.sender, toSecond, valueSecond);
     }
